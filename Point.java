2dc6cf6c75b4e5cc5862b7a0b055f9433aa8917d
Joe <Joe.1656344531@gmail.com>
2017-03-05 23:44:51 +0800

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,78 @@
+package ML.Kmeans.Kmeans_opt;
+
+/**
+ * Created by xwz on 3/2/17.
+ *
+ * Point: 点坐标
+ */
+
+public class Point {
+    private double x; //X坐标
+    private double y; //Y坐标
+    private int id;
+    private boolean beyond;//标识是否属于样本
+
+    //构造函数
+    public Point(int id, double x, double y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.beyond = true;
+    }
+
+    public Point(int id, double x, double y, boolean beyond) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.beyond = beyond;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isBeyond() {
+        return beyond;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "id=" + id +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (Double.compare(point.x, x) != 0) return false;
+        if (Double.compare(point.y, y) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = x != +0.0d ? Double.doubleToLongBits(x) : 0L;
+        result = (int) (temp ^ (temp >>> 32));
+        temp = y != +0.0d ? Double.doubleToLongBits(y) : 0L;
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
